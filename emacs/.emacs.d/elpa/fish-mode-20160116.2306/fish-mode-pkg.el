5feaaa2e57ba3aad5d8bdbff4de3ec93ce671e9e
TaffyQuinzel <louis.burg@hotmail.com>
2016-01-22 13:51:07 +0100

    update emacs

---
+++ emacs/.emacs.d/elpa/fish-mode-20160116.2306/fish-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "fish-mode" "20160116.2306" "Major mode for fish shell scripts" '((emacs "24")) :keywords '("fish" "shell"))
---
+++ emacs/.emacs.d/elpa/slime-annot-20131230.1908/slime-annot-autoloads.el
@@ -0,0 +1,15 @@
+;;; slime-annot-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("slime-annot.el") (22178 6604 808565 319000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; slime-annot-autoloads.el ends here
