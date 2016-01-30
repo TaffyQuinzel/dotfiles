3c6d2a3875e30088fbbb17116ee55e9ff4e7b079
TaffyQuinzel <louis.burg@hotmail.com>
2016-01-31 00:41:23 +0100

    made ssh agent working like it should :3

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,10 @@
 if status --is-login
     set -xg PANEL_FIFO /tmp/panel-fifo
-    set -xg XDG_DATA_HOME $HOME/.data
     set -xg MOZ_PLUGIN_PATH /usr/lib/mozilla/plugins
     set -xg MAIL /var/spool/mail/$USER
-    set PATH $PATH /usr/local/sbin /usr/local/bin /usr/bin /usr/lib/jvm/default/bin /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
+    set PATH $PATH ~/.bin /usr/local/sbin /usr/local/bin /usr/bin /usr/lib/jvm/default/bin /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
     set -xg LANG en_GB.UTF-8
+    set -xg XDG_DATA_HOME $HOME/.data
     set -xg XDG_CONFIG_HOME $HOME/.config
     set -xg XDG_CACHE_HOME $HOME/.cache
     set -xg _ /usr/bin/printenv
@@ -13,7 +13,7 @@ if status --is-login
     set -xg BROWSER firefox
     set -xg JAVA_HOME /usr/lib/jvm/java-7-openjdk/jre
     set -xg CATALINA_HOME /opt/tomcat
-    set -xg SSH_AUTH_SOCK /tmp/ssh-agent.socket
+    set -xg SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 end
 
 #remove welcome message
@@ -27,6 +27,7 @@ alias svim='sudo vim'
 alias logout='exit'
 alias umount='sudo umount'
 alias emacs='emacsclient -c'
+alias startx='ssh-agent startx'
 #system specific
 if [ (hostname) = "genetic" ]
     alias install='sudo emerge --ask'
@@ -65,9 +66,9 @@ function aoeu -d 'Correct your previous console command'
     thefuck $fucked_up_commandd > $eval_script
     . $eval_script
     rm $eval_script
-    if test $exit_code -ne 0
-        history --delete $fucked_up_commandd
-    end
+    # if test $exit_code -ne 0
+    #     history --delete $fucked_up_commandd
+    # end
 end
 
 #THE BEST THING EVER!!!!!!!!!!!!!
